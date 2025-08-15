925586f4d8c3542f91fea733d934d7513d96bcca
joshua <joshua.kistler@swisscom.com>
2025-08-15 10:03:03 +0200

    FirstPush for Task manager app

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+
+
+const Header = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+} 
+
+Header.defaultProps = {
+  title: "Task Tracker"
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+/*  CSS in JS
+const headingStyle = {
+  color: 'red', 
+  backgroundColor: 'black'
+}
+*/
+export default Header
